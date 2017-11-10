d063f17b1b281f785bca5e4983ba98ab437f2f93
Guillaume Allais <guillaume.allais@ens-lyon.org>
2017-11-10 15:08:00 +0100

    [ new ] Sized Strings as a bunch of asciis

--- src/Sized.v
+++ src/Sized.v
@@ -1,4 +1,4 @@
-Require Import Indexed.
+Require Import PeanoNat.
 
 Definition View (As : nat -> Type) (A : Type) (n : nat) : Type :=
 match n with
@@ -23,3 +23,30 @@ Instance sizedVector {A : Type} : Sized (Vector.t A) A :=
     | O    => fun _ => None
     | S n' => fun v => Some (hd v, tl v)
   end).
+
+Require Import Ascii.
+Require Import String.
+
+Record SizedType {A : Type} (size : A -> nat) (n : nat) : Type :=
+  MkSizedType { value : A
+           ; sized : size value = n
+           }.
+
+Arguments MkSizedType {_} {_} {_}.
+
+Definition SizedString n := SizedType length n.
+
+Definition mkString (str : string) : SizedString (length str) := MkSizedType str (eq_refl _).
+
+Definition stringUncons {n} (Str : SizedString (S n)) : ascii * SizedString n :=
+  let (str, prf) := Str in
+  (match str return length str = S n -> ascii * SizedString n with
+   | EmptyString => fun hf  => False_rect _ (Nat.neq_0_succ _ hf)
+   | String c s' => fun Seq => pair c (MkSizedType s' (Nat.succ_inj _ _ Seq))
+  end) prf.
+
+Instance sizedString : Sized SizedString ascii := MkSized (fun n =>
+match n return SizedString n -> option (View _ _ n) with
+ | O    => fun _   => None
+ | S n' => fun str => Some (stringUncons str)
+end).
